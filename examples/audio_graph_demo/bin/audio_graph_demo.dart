b2fa593fca5bc5fce4b2735a628c112ef4bf7a36
Kaisei Sunaga <skkbyssk@gmail.com>
2023-02-22 02:11:44 +0900

    add wave audio graph demo

---
+++ examples/audio_graph_demo/bin/audio_graph_demo.dart
@@ -0,0 +1,5 @@
+import 'package:audio_graph_demo/main.dart' as audio_graph_demo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${audio_graph_demo.calculate()}!');
+}
---
+++ examples/audio_graph_demo/test/audio_graph_demo_test.dart
@@ -0,0 +1,8 @@
+import 'package:audio_graph_demo/main.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
