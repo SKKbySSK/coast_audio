8cfef8a1ad92e4972e8b6a24e39a9b999885296c
kaisei-sunaga <skkbyssk@gmail.com>
2024-02-25 16:12:48 +0900

    add volume node test

---
+++ packages/coast_audio/test/node/volume_node_test.dart
@@ -0,0 +1,55 @@
+import 'package:coast_audio/coast_audio.dart';
+import 'package:test/test.dart';
+
+class SourceNode extends DataSourceNode {
+  SourceNode({required this.outputFormat});
+
+  @override
+  final AudioFormat outputFormat;
+
+  @override
+  AudioReadResult read(AudioOutputBus outputBus, AudioBuffer buffer) {
+    Memory().setMemory(buffer.pBuffer.cast(), 1, buffer.sizeInBytes);
+    return AudioReadResult(frameCount: buffer.sizeInFrames, isEnd: false);
+  }
+}
+
+void main() {
+  group('VolumeNode', () {
+    for (final sampleFormat in SampleFormat.values) {
+      final format = AudioFormat(sampleRate: 44100, channels: 2, sampleFormat: sampleFormat);
+
+      test('[${sampleFormat.name}] set volume to 0%', () {
+        final source = SourceNode(outputFormat: format);
+        final volume = VolumeNode(volume: 0);
+        final graph = GraphNode()
+          ..connect(source.outputBus, volume.inputBus)
+          ..connectEndpoint(volume.outputBus);
+
+        final frames = AllocatedAudioFrames(length: 100, format: format);
+        frames.acquireBuffer((buffer) {
+          graph.outputBus.read(buffer);
+
+          final list = buffer.asUint8ListViewBytes();
+          expect(list.every((element) => element == sampleFormat.mid), isTrue);
+        });
+      });
+
+      test('[${sampleFormat.name}] set volume to 100%', () {
+        final source = SourceNode(outputFormat: format);
+        final volume = VolumeNode(volume: 1);
+        final graph = GraphNode()
+          ..connect(source.outputBus, volume.inputBus)
+          ..connectEndpoint(volume.outputBus);
+
+        final frames = AllocatedAudioFrames(length: 100, format: format);
+        frames.acquireBuffer((buffer) {
+          graph.outputBus.read(buffer);
+
+          final list = buffer.asUint8ListViewBytes();
+          expect(list.every((element) => element == 1), isTrue);
+        });
+      });
+    }
+  });
+}
